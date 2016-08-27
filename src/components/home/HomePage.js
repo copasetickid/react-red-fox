45765f9a4b8af7f4406f26aac94ecf0d395e4151
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-27 14:25:42 -0400

    Update homepage copy

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="section no-pad-bot" id="index-banner">
         <div className="container">
-          <h1 className="header center orange-text">Inventory</h1>
+          <h1 className="header center green-text">Inventory</h1>
           <div className="row center">
             <h5 className="header col s12 light">A place to check to store inventory</h5>
+          </div>
+          <div className="row center">
+            <Link to="/" className="waves-effect waves-light btn-large green">View Stores</Link>
           </div>
         </div>
       </div>
