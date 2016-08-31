95917adeb4988cffcbdc757c095d69f1a7d2cbb6
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-31 11:42:36 -0400

    Creates a link to the new store page from the stores page

--- src/components/store/StoresPage.js
+++ src/components/store/StoresPage.js
@@ -4,25 +4,33 @@ import { bindActionCreators } from 'redux';
 import { fetchShops } from '../../actions/shopAction';
 import Preloader from '../shared/Preloader';
 import StoreCard from './StoreCard';
+import { browserHistory } from 'react-router';
 
 class StoresPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToNewStorePage = this.redirectToNewStorePage.bind(this);
   }
 
+  redirectToNewStorePage() {
+    browserHistory.push('newstore');
+  }
   render() {
     const { shops } = this.props;
-    
+
     return (
-      <div className="stores-container">
+      <div className="container stores-container">
 
         <div className="row">
           { shops.map(shop =>
             <StoreCard key={shop.id} shop={shop} />
           )}
         </div>
-
+        <a className="btn-floating btn-large btn-add waves-effect waves-light red" onClick={this.redirectToNewStorePage}>
+            <i className="material-icons">add</i>
+        </a>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import StoresPage from './components/store/StoresPage';
+import CreateStorePage from './components/store/CreateStorePage';
 
 
 export default (
   <Route path="/" component={App}>
       <IndexRoute component={HomePage} />
       <Route path="stores" component={StoresPage}/>
+      <Route path="newstore" component={CreateStorePage} />
   </Route>
 );
